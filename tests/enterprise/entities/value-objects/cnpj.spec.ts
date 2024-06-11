967aeb0550dbf980ea569dd61cb8fb1eab4acce1
Matheus Torres <torresmiranda.rj@gmail.com>
2024-06-11 16:26:48 -0300

    test: ensure SUT creates a valid CNPJ

--- tests/enterprise/entities/value-objects/cnpj.spec.ts
+++ tests/enterprise/entities/value-objects/cnpj.spec.ts
@@ -1,5 +1,7 @@
-import { throws } from 'node:assert';
+import { deepStrictEqual, throws } from 'node:assert';
 import { describe, it } from 'node:test';
+
+import { clearString } from '@/utils/funcs/clear-string';
 
 import { CNPJ } from '@/enterprise/entities/value-objects/cnpj';
 import { CNPJIsInvalidError } from '@/enterprise/errors/cnpj';
@@ -7,5 +9,12 @@ import { CNPJIsInvalidError } from '@/enterprise/errors/cnpj';
 describe('CNPJ', () => {
 	it('should throw if an invalid CNPJ is provided', () => {
 		throws(() => CNPJ.create('12312312312'), new CNPJIsInvalidError());
+	});
+
+	it('should create a valid CNPJ', () => {
+		const data = '06.108.507/0001-40';
+		const cnpj = CNPJ.create(data);
+
+		deepStrictEqual(cnpj.props.value, clearString(data));
 	});
 });
