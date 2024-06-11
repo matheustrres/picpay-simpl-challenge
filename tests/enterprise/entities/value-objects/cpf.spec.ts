fdffa9a66c2b9397df1ad563b01f05c9400e260a
Matheus Torres <torresmiranda.rj@gmail.com>
2024-06-11 14:16:26 -0300

    test: ensure SUT creates a valid CPF

--- tests/enterprise/entities/value-objects/cpf.spec.ts
+++ tests/enterprise/entities/value-objects/cpf.spec.ts
@@ -1,5 +1,7 @@
-import { throws } from 'node:assert';
+import { deepStrictEqual, throws } from 'node:assert';
 import { describe, it } from 'node:test';
+
+import { clearString } from '@/utils/funcs/clear-string';
 
 import { CPF } from '@/enterprise/entities/value-objects/cpf';
 import { CPFIsInvalidError } from '@/enterprise/errors/cpf';
@@ -7,5 +9,12 @@ import { CPFIsInvalidError } from '@/enterprise/errors/cpf';
 describe('CPF', () => {
 	it('should throw if an invalid CPF is provided', () => {
 		throws(() => CPF.create('11257245286'), new CPFIsInvalidError());
+	});
+
+	it('should create a valid CPF', () => {
+		const data = '298.942.410-50';
+		const cpf = CPF.create(data);
+
+		deepStrictEqual(cpf.props.value, clearString(data));
 	});
 });
