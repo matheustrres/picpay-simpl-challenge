db37c48095ad2e7f6bea2af646c86fe23c34510c
Matheus Torres <torresmiranda.rj@gmail.com>
2024-06-12 11:18:04 -0300

    test: ensure SUT restores a Wallet

--- tests/enterprise/entities/wallet/wallet.spec.ts
+++ tests/enterprise/entities/wallet/wallet.spec.ts
@@ -1,6 +1,8 @@
 import { deepStrictEqual } from 'node:assert';
 import { describe, it } from 'node:test';
 
+import { Wallet } from '@/enterprise/entities/wallet';
+
 import { WalletBuilder } from '#/data/builders/entities/wallet/wallet';
 
 describe('Wallet', () => {
@@ -8,5 +10,16 @@ describe('Wallet', () => {
 		const wallet = new WalletBuilder().withBalance(2_000).build();
 
 		deepStrictEqual(wallet.getProps().balance, 2_000);
+	});
+
+	it('should restore a Wallet', () => {
+		const wallet = new WalletBuilder().build();
+
+		const restoredWallet = Wallet.restore({
+			id: wallet.id,
+			props: wallet.getProps(),
+		});
+
+		deepStrictEqual(restoredWallet, wallet);
 	});
 });
