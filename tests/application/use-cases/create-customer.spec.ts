a0e16503f6a37ae74d515d534ccb66c150e9fe84
Matheus Torres <torresmiranda.rj@gmail.com>
2024-06-13 19:17:54 -0300

    refactor(test): remove duplicated tests

--- tests/application/use-cases/create-customer.spec.ts
+++ tests/application/use-cases/create-customer.spec.ts
@@ -1,14 +1,10 @@
 import { deepStrictEqual, strictEqual } from 'node:assert';
 import { describe, it } from 'node:test';
 
-import { UserAlreadyExistsError } from '@/application/errors/user';
 import { CreateCustomerUseCase } from '@/application/use-cases/create-customer';
 
 import { Customer } from '@/enterprise/entities/user/customer';
-import { CPFError } from '@/enterprise/errors/cpf';
-import { EmailError } from '@/enterprise/errors/email';
 
-import { CustomerBuilder } from '#/data/builders/entities/user/customer';
 import { CreateCustomerUseCaseBuilder } from '#/data/builders/use-cases/create-customer';
 import {
 	type MockedHashProvider,
@@ -37,68 +33,6 @@ function makeSUT(): SUT {
 }
 
 describe('CreateCustomerUseCase', () => {
-	it('should return CPFError if CPF creation fails [LEFT]', async () => {
-		const { sut } = makeSUT();
-
-		const { isLeft, isRight, value } = await sut.exec({
-			...new CreateCustomerUseCaseBuilder().getProps(),
-			cpf: '11257245286',
-		});
-
-		strictEqual(isLeft(), true);
-		strictEqual(isRight(), false);
-		strictEqual(value instanceof CPFError, true);
-	});
-
-	it('should return EmailError if email creation fails [LEFT]', async () => {
-		const { sut } = makeSUT();
-
-		const { isLeft, isRight, value } = await sut.exec({
-			...new CreateCustomerUseCaseBuilder().getProps(),
-			email: 'adam_smith@.br',
-		});
-
-		strictEqual(isLeft(), true);
-		strictEqual(isRight(), false);
-		strictEqual(value instanceof EmailError, true);
-	});
-
-	it('should return UserAlreadyExistsError if given CPF is already in use [LEFT]', async () => {
-		const { sut, usersRepository } = makeSUT();
-
-		const user = new CustomerBuilder().build();
-
-		usersRepository.findByCPF.mock.mockImplementationOnce(() => user);
-
-		const { isLeft, isRight, value } = await sut.exec(
-			new CreateCustomerUseCaseBuilder()
-				.withCPF(user.getProps().CPF.props.value)
-				.getProps(),
-		);
-
-		strictEqual(isLeft(), true);
-		strictEqual(isRight(), false);
-		strictEqual(value instanceof UserAlreadyExistsError, true);
-	});
-
-	it('should return UserAlreadyExistsError if given email address is already in use [LEFT]', async () => {
-		const { sut, usersRepository } = makeSUT();
-
-		const user = new CustomerBuilder().build();
-
-		usersRepository.findByEmail.mock.mockImplementationOnce(() => user);
-
-		const { isLeft, isRight, value } = await sut.exec(
-			new CreateCustomerUseCaseBuilder()
-				.withEmail(user.getProps().email.props.value)
-				.getProps(),
-		);
-
-		strictEqual(isLeft(), true);
-		strictEqual(isRight(), false);
-		strictEqual(value instanceof UserAlreadyExistsError, true);
-	});
-
 	it('should return a Customer [RIGHT]', async () => {
 		const { sut, usersRepository } = makeSUT();
 
