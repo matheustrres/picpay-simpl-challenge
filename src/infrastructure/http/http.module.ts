11ed76caff26abb861a331d66fc5b5109e02cd5c
Matheus Torres <torresmiranda.rj@gmail.com>
2024-06-17 17:46:52 -0300

    chore: move adapters into gateways/ folder

--- src/infrastructure/http/http.module.ts
+++ src/infrastructure/http/http.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
-import { CryptographyModule } from '@/infrastructure/adapters/cryptography/cryptography.module';
-import { HashingModule } from '@/infrastructure/adapters/hashing/hashing.module';
+import { CryptographyModule } from '@/infrastructure/gateways/adapters/cryptography/cryptography.module';
+import { HashingModule } from '@/infrastructure/gateways/adapters/hashing/hashing.module';
 
 @Module({
 	imports: [CryptographyModule, HashingModule],
