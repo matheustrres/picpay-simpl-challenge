a019dac3435bee3e04a962449590ddfac64d8ee3
Matheus Torres <torresmiranda.rj@gmail.com>
2024-06-20 16:09:40 -0300

    chore: set Customer & Shopkeeper wallet relation as optional

---
+++ prisma/migrations/20240620190916_set_wallet_as_optional_for_user/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "customers" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "shopkeepers" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "wallets" ALTER COLUMN "updated_at" SET DEFAULT CURRENT_TIMESTAMP;
